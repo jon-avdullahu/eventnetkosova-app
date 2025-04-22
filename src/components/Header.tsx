180c8251806f5e5d23d53dfd34adce875567c328
Jon Avdullahu <81752726+jon-avdullahu@users.noreply.github.com>
2025-04-22 21:55:07 +0200

    updated.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,9 +31,6 @@ const Header = () => {
           <Link href="#pricing" className="text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400">
             Pricing
           </Link>
-          <Link href="#team" className="text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400">
-            Our Team
-          </Link>
           <Link href="#about" className="text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400">
             About
           </Link>
@@ -83,13 +80,6 @@ const Header = () => {
                 onClick={() => setIsOpen(false)}
               >
                 Pricing
-              </Link>
-              <Link 
-                href="#team" 
-                className="text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400 py-2"
-                onClick={() => setIsOpen(false)}
-              >
-                Our Team
               </Link>
               <Link 
                 href="#about" 
